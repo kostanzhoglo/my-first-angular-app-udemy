ea975a673ed7721ad1be498b9c0eaf1a481895e6
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-25 07:45:22 -0500

    Clean slate, deleted code from Sect 1, lect 6, and added Bootstrap to project and add as 'styles' dependency in angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
